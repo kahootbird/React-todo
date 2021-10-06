6184fce4aa635134557aaff095a6053ec3f2ae08
Ronnie Goodrich <kahootbird@gmail.com>
2021-10-05 22:16:43 -0400

    Updated CSS; Detect click event

--- src/App.js
+++ src/App.js
@@ -6,23 +6,29 @@ import Todo1 from './components/Todo1'
 //import Todo1 from './components/Todo1'
 
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    //alert('A name was submitted: ' + this.event.value);
+    console.log("VALUE SUBMITTED")
+    //event.preventDefault();
+  }
   return (
     <div className="App">
       <header className="App-header">
         Todo List
         <Component1 title='Hello From Title Prop' />
         <Todo1 title='Todo List' />
-
+        <form onSubmit={handleSubmit}>
+        <fieldset>
+           <label>
+             <p>Name</p>
+             <input name="name" />
+           </label>
+         </fieldset>
+                 <input type="submit" value="Submit" />
+         </form>
       </header>
-      <form>
-      <fieldset>
-         <label>
-           <p>Name</p>
-           <input name="name" />
-         </label>
-       </fieldset>
-       <button type="submit">Submit</button>
-      </form>
+
     </div>
   );
 }
